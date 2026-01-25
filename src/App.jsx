247d08f0e05fb0a37d0b5e59ac33c2655ba4e6b2
Lidia Pogrebniak <akforever@rambler.ru>
2026-01-25 09:45:48 +0200

    react+vite+bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar.jsx';
+import Home from './components/Home.jsx';
+
+function App() {
+  return (
+    <>
+        <NavBar />
+        <Home />
+    </>
+  );
+}
+
+export default App;
